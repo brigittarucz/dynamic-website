00dfd709ecc32a50ec22a4e58837255652699288
Brigitta Rucz <me@example.com>
2019-02-15 10:14:44 +0100

    showDetails button not working

--- javascript.js
+++ javascript.js
@@ -20,6 +20,7 @@ var database = [
 
 let productList = "http://kea-alt-del.dk/t5/api/productlist";
 let categoriesList = "http://kea-alt-del.dk/t5/api/categories"
+let specificItemList = "http://kea-alt-del.dk/t5/api/product?id=";
 
 let promise = fetch(categoriesList)
 .then(e=>e.json())
@@ -58,10 +59,8 @@ function showCategory(category) {
                 divsSelected.forEach(div => {
                     if (div.id == "flexboxGrid" + category) {
                         div.style.display = "flex";
-                        console.log("success");
                     } else {
                         div.style.display = "none";
-                        console.log(div.id);
                     }
                 });
             };
@@ -77,20 +76,7 @@ function showAll() {
     });
 };
 
-/* let allSelected = document.querySelector(".all");
-let divsSelected = document.querySelectorAll("[id^='flexboxGrid']");
-console.log(divsSelected);
-allSelected.addEventListener("click", showAll);
-
-function showAll() {
-
-     divsSelected.style.display = "flex";
 }
-*/
-
-}
-
-
 
 function loadData(productList) {
 
@@ -109,6 +95,68 @@ function functionName(elementTable) {
      const parentElem = document.getElementById("flexboxGrid" + elementTable.category);
 
     const clone = template.cloneNode(true);
+
+
+function fetchById() {
+
+     let modal = clone.querySelector(".modalContent");
+     let parentModal = clone.querySelector("#modalTemplate");
+fetch(specificItemList + elementTable.id)
+.then(e=>e.json())
+.then(info=>{
+    /* Create here the modals and add an id */
+    /* Add to each openButton class an id */
+    /* You create a function to unhide by searching for id the specific modal */
+    modal.querySelector("h3").textContent = info.name;
+    modal.querySelector("p").textContent = info.longdescription;
+    modal.querySelector("img").setAttribute('src', ("imgs/large/" + info.image + ".jpg"));
+    modal.querySelector("#number").textContent = info.stars;
+    modal.querySelector("#region").textContent = info.region;
+    modal.querySelector(".price").textContent = info.price + " dkk";
+
+    if(info.vegetarian) {
+            modal.querySelector(".veg").setAttribute('src',"imgs/vegLogo@0,25x.png");
+
+    }
+
+    if(info.discount) {
+         modal.querySelector(".price").classList.add("discount");
+        modal.querySelector(".newText").textContent = info.discount + "dkk";
+
+    }
+
+   if(info.soldout) {
+       modal.querySelector(".price").classList.add("soldOut");
+        modal.querySelector(".newText").textContent = "Sold Out";
+
+   }
+
+    if(info.alcohol) {
+
+    modal.querySelector("#alcohol").textContent = "Alcohol: " + info.alcohol + "%";
+
+    }
+
+    if(info.allergens) {
+
+        modal.querySelector("#allergens").textContent = "Allergens: " + info.allergens;
+    }
+
+    parentModal.setAttribute("class", info.id);
+    parentModal.classList.add("hide");
+
+})
+    };
+    fetchById();
+
+    let button = clone.querySelector(".openButton");
+    let modalTempl = clone.getElementById("#modalTemplate");
+
+    button.setAttribute("id", elementTable.id);
+    button.classList.add("openButton");
+
+
+
     clone.querySelector("h3").textContent = elementTable.name;
     clone.querySelector("#description").textContent = elementTable.shortdescription;
     clone.querySelector("img").setAttribute('src', ("imgs/large/" + elementTable.image + ".jpg"));
@@ -155,6 +203,41 @@ function functionName(elementTable) {
 }
 
 loadData(productList);
+
+/*
+setTimeout(function() {
+    let button = document.querySelector(".openButton");
+    let modalTempl = document.getElementById("#modalTemplate");
+
+    button.forEach(function(elem) { elem.addEventListener("click", openModal);
+
+    function openModal() {
+        if (elem.class == modalTempl.class) {
+        console.log("success"); }
+        else {
+            console.log("fail");
+        }
+    }
+
+},3000);
+
+let buttons = document.querySelectorAll(".openButton");
+let modals = document.querySelectorAll("#modalTemplate");
+
+buttons.forEach(function(elem)) {
+    elem.addEventListener("click", ()=>
+    if(elem.id == (modals.forEach(function(elem){
+    return elem.class;
+    }))) {
+    console.log("success");
+    } else {
+    console.log("fail");
+    }
+    )
+};
+
+
+*/
 
 var numberStars = document.querySelectorAll("#number");
 var arrayLength = numberStars.length;
